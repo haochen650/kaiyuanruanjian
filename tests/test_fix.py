530d4fd325e4cdbb58668c6426fc8f20e4726821
chen <1198866090@qq.com>
2026-02-11 12:12:38 +0800

    Add files via upload

---
+++ tests/test_fix.py
@@ -0,0 +1,10 @@
+import os
+import sys as system_api  # 测试别名
+import datetime
+import pandas as pd        # 测试别名
+from collections import defaultdict, Counter
+
+def test():
+    # 只使用 os 和 defaultdict
+    print(os.name)
+    d = defaultdict(int)
